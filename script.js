7400e24d4482e2c4dcf38447be5423364613c839
Niraj Modi <modiniraj1034@gmail.com>
2024-08-01 15:43:23 +0530

    Updated Icon

--- script.js
+++ script.js
@@ -3,45 +3,45 @@ const sun = document.querySelector(".fa-sun");
 const moon = document.querySelector(".fa-moon");
 const sunMoon = document.querySelector(".sunMoon");
 sun.addEventListener("click", () => {
-  sunMoon.style.animationName = "rotate-90";
-  home.classList.add("dayImage");
-  setTimeout(() => {
-    document.documentElement.style.setProperty("--up", "#00c4ff");
-    document.documentElement.style.setProperty("--down", "#0512c7");
-    document.documentElement.style.setProperty("--text-color", "rgb(30,20,19)");
-    document.documentElement.style.setProperty(
-      "--secondary-text-color",
-      "rgb(44,44,44)"
-    );
-  }, 800);
+	sunMoon.style.animationName = "rotate-90";
+	home.classList.add("dayImage");
+	setTimeout(() => {
+		document.documentElement.style.setProperty("--up", "#00c4ff");
+		document.documentElement.style.setProperty("--down", "#0512c7");
+		document.documentElement.style.setProperty("--text-color", "rgb(30,20,19)");
+		document.documentElement.style.setProperty(
+			"--secondary-text-color",
+			"rgb(44,44,44)"
+		);
+	}, 800);
 });
 moon.addEventListener("click", () => {
-  sunMoon.style.animationName = "rotate90";
-  home.classList.remove("dayImage");
-  setTimeout(() => {
-    document.documentElement.style.setProperty("--up", "#023d96");
-    document.documentElement.style.setProperty("--down", "rgb(1, 0, 75)");
-    document.documentElement.style.setProperty("--text-color", "white");
-    document.documentElement.style.setProperty(
-      "--secondary-text-color",
-      "rgb(144, 144, 144)"
-    );
-  }, 800);
+	sunMoon.style.animationName = "rotate90";
+	home.classList.remove("dayImage");
+	setTimeout(() => {
+		document.documentElement.style.setProperty("--up", "#023d96");
+		document.documentElement.style.setProperty("--down", "rgb(1, 0, 75)");
+		document.documentElement.style.setProperty("--text-color", "white");
+		document.documentElement.style.setProperty(
+			"--secondary-text-color",
+			"rgb(144, 144, 144)"
+		);
+	}, 800);
 });
 //  Create Bubble Starts Here
 function createBubble() {
-  const section = document.querySelector(".seaBack");
-  const createElement = document.createElement("span");
-  var size = Math.random() * 30;
+	const section = document.querySelector(".seaBack");
+	const createElement = document.createElement("span");
+	var size = Math.random() * 30;
 
-  createElement.style.width = 15 + size + "px";
-  createElement.style.height = 15 + size + "px";
-  createElement.style.left = Math.random() * innerWidth + "px";
-  section.appendChild(createElement);
+	createElement.style.width = 15 + size + "px";
+	createElement.style.height = 15 + size + "px";
+	createElement.style.left = Math.random() * innerWidth + "px";
+	section.appendChild(createElement);
 
-  setTimeout(() => {
-    createElement.remove();
-  }, 6500);
+	setTimeout(() => {
+		createElement.remove();
+	}, 6500);
 }
 setInterval(createBubble, 150);
 //  Create Bubble Ends Here
@@ -49,7 +49,7 @@ setInterval(createBubble, 150);
 // Setting specific project  height for the image dimension
 const projectContainer = document.getElementsByClassName("projectContainer");
 for (let ele of projectContainer) {
-  ele.style.height = `${0.52 * +ele.offsetWidth}px`;
+	ele.style.height = `${0.52 * +ele.offsetWidth}px`;
 }
 
 // About Me Link At end of the page
@@ -57,12 +57,12 @@ for (let ele of projectContainer) {
 const aboutmeLink = document.querySelector("#aboutme-link");
 const upButton = document.querySelector("#upButton");
 const handleClickOnAboutMe = (event) => {
-  event.preventDefault();
-  const aboutmePosition = document.querySelector("#main").offsetTop;
-  document.querySelector("#main").scrollTo({
-    top: aboutmePosition,
-    behavior: "smooth",
-  });
+	event.preventDefault();
+	const aboutmePosition = document.querySelector("#main").offsetTop;
+	document.querySelector("#main").scrollTo({
+		top: aboutmePosition,
+		behavior: "smooth",
+	});
 };
 aboutmeLink.addEventListener("click", handleClickOnAboutMe);
 upButton.addEventListener("click", handleClickOnAboutMe);
@@ -78,90 +78,90 @@ const socialLinks = document.querySelectorAll(".linksToHandles");
 const inputBoxes = document.querySelectorAll(".inputBox");
 
 containerElement.addEventListener("scroll", () => {
-  const bounding = mainElement.getBoundingClientRect();
-  const containerHeight = containerElement.offsetHeight;
-  if (bounding.top < containerHeight) {
-    contentHeadings[0].classList.add("reveal");
-    leftBoxes[0].classList.add("reveal");
-    rightBoxes[0].classList.add("reveal");
-  } else {
-    contentHeadings[0].classList.remove("reveal");
-    leftBoxes[0].classList.remove("reveal");
-    rightBoxes[0].classList.remove("reveal");
-  }
+	const bounding = mainElement.getBoundingClientRect();
+	const containerHeight = containerElement.offsetHeight;
+	if (bounding.top < containerHeight) {
+		contentHeadings[0].classList.add("reveal");
+		leftBoxes[0].classList.add("reveal");
+		rightBoxes[0].classList.add("reveal");
+	} else {
+		contentHeadings[0].classList.remove("reveal");
+		leftBoxes[0].classList.remove("reveal");
+		rightBoxes[0].classList.remove("reveal");
+	}
 });
 
 function scrollToReveal(items, revealPoint) {
-  for (let i of items) {
-    var mainHeight = window.innerHeight;
-    var revealTop = i.getBoundingClientRect().top;
-    if (revealTop < mainHeight - revealPoint) {
-      i.classList.add("reveal");
-    } else {
-      i.classList.remove("reveal");
-    }
-  }
+	for (let i of items) {
+		var mainHeight = window.innerHeight;
+		var revealTop = i.getBoundingClientRect().top;
+		if (revealTop < mainHeight - revealPoint) {
+			i.classList.add("reveal");
+		} else {
+			i.classList.remove("reveal");
+		}
+	}
 }
 
 mainElement.addEventListener("scroll", () => {
-  scrollToReveal(contentHeadings, 100);
-  scrollToReveal(institutions, 0);
-  scrollToReveal(leftBoxes, 200);
-  scrollToReveal(rightBoxes, 200);
-  scrollToReveal(socialLinks, 50);
-  scrollToReveal(inputBoxes, 0);
+	scrollToReveal(contentHeadings, 100);
+	scrollToReveal(institutions, 0);
+	scrollToReveal(leftBoxes, 200);
+	scrollToReveal(rightBoxes, 200);
+	scrollToReveal(socialLinks, 50);
+	scrollToReveal(inputBoxes, 0);
 });
 
 // TypeWriter Effect
 function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function typeWriter(strings, object, key) {
-  let stringIndex = 0;
-  let charIndex = 0;
-  let isForward = true; // Flag to indicate the direction of printing
-  const originalTitle = object[key][object[key].length - 1];
+	let stringIndex = 0;
+	let charIndex = 0;
+	let isForward = true; // Flag to indicate the direction of printing
+	const originalTitle = object[key][object[key].length - 1] || "";
 
-  async function cursorBlinking(currString) {
-    let round = 2;
-    while (round--) {
-      currString = currString.substring(0, currString.length - 1);
-      object[key] = currString;
-      await delay(400);
-      currString += "|";
-      object[key] = currString;
-      await delay(400);
-    }
-  }
-  while (true) {
-    const currentString = strings[stringIndex];
+	async function cursorBlinking(currString) {
+		let round = 2;
+		while (round--) {
+			currString = currString.substring(0, currString.length - 1);
+			object[key] = currString;
+			await delay(400);
+			currString += "|";
+			object[key] = currString;
+			await delay(400);
+		}
+	}
+	while (true) {
+		const currentString = strings[stringIndex];
 
-    if (charIndex >= 0 && charIndex < currentString.length && isForward) {
-      object[key] = currentString.substring(0, charIndex + 1) + "|";
-      charIndex++;
-      await delay(300); // Delay between characters while writing
-    } else if (charIndex >= 0 && !isForward) {
-      if (charIndex) {
-        object[key] = currentString.substring(0, charIndex) + "|";
-        await delay(250); // Delay between characters while deleting
-      } else {
-        await cursorBlinking(originalTitle + "|");
-      }
-      charIndex--;
-    } else {
-      if (isForward) {
-        isForward = false;
-        charIndex--;
-        let currValue = object[key];
-        await cursorBlinking(currValue);
-      } else {
-        isForward = true;
-        stringIndex = (stringIndex + 1) % strings.length;
-        charIndex++;
-      }
-    }
-  }
+		if (charIndex >= 0 && charIndex < currentString.length && isForward) {
+			object[key] = currentString.substring(0, charIndex + 1) + "|";
+			charIndex++;
+			await delay(300); // Delay between characters while writing
+		} else if (charIndex >= 0 && !isForward) {
+			if (charIndex) {
+				object[key] = currentString.substring(0, charIndex) + "|";
+				await delay(250); // Delay between characters while deleting
+			} else {
+				await cursorBlinking(originalTitle + "|");
+			}
+			charIndex--;
+		} else {
+			if (isForward) {
+				isForward = false;
+				charIndex--;
+				let currValue = object[key];
+				await cursorBlinking(currValue);
+			} else {
+				isForward = true;
+				stringIndex = (stringIndex + 1) % strings.length;
+				charIndex++;
+			}
+		}
+	}
 }
 
 // TypeWriter Effect Ends Here
@@ -176,8 +176,8 @@ typeWriter(titles, document, "title");
 //Setting Position / Title
 
 const position = [
-  "a FullStack Developer",
-  "a Community Lead",
-  "TheCodeVenturer",
+	"a FullStack Developer",
+	"a Community Lead",
+	"TheCodeVenturer",
 ];
-typeWriter(position, document.querySelector("#myTitle"), "innerHTML");
+typeWriter(position, document.querySelector("#myTitle"), "innerText");
